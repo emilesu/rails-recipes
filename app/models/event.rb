66383f33e6b909e8eff8e605c2feb9f227f0513e
emilesu <emilesu@qq.com>
2017-06-20 20:58:13 +0800

    add status column

--- app/models/event.rb
+++ app/models/event.rb
@@ -17,4 +17,7 @@ class Event < ApplicationRecord
    self.friendly_id ||= SecureRandom.uuid
  end
 
+ STATUS = ["draft", "public", "private"]
+ validates_inclusion_of :status, :in => STATUS      #status的范围
+
 end
