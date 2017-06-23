ffa8597b3bc49216edd29699ea05b6aa41486f8c
emilesu <emilesu@qq.com>
2017-06-24 01:13:53 +0800

    complement profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :find_user
+
+  def show
+  end
 
   def edit
     @user = current_user
+    @user.create_profile unless @user.profile
   end
 
   def update
@@ -11,7 +16,7 @@ class UsersController < ApplicationController
 
     if @user.update(user_params)
       flash[:notice] = "修改成功"
-      redirect_to edit_user_path
+      redirect_to user_path
     else
       render "edit"
     end
@@ -19,8 +24,13 @@ class UsersController < ApplicationController
 
   protected
 
+  def find_user
+    @user = current_user
+    @user.create_profile unless @user.profile
+  end
+
   def user_params
-    params.require(:user).permit(:time_zone)
+    params.require(:user).permit(:time_zone, :profile_attributes => [:id, :legal_name, :birthday, :location, :education, :occupation, :bio, :specialty] )
   end
 
 
