b0cc93eadf15c9e7a5fd8eff76d3d367b0dbd29e
emilesu <emilesu@qq.com>
2017-07-08 19:05:52 +0800

    validate complement

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,6 +3,7 @@ class Registration < ApplicationRecord
   STATUS = ["pending", "confirmed"]
   validates_inclusion_of :status, :in => STATUS         #验证只有某些值
   validates_presence_of :status, :ticket_id
+  validate :check_event_status, :on => :create
 
   attr_accessor :current_step         #attr_accessor 是一个虚拟属性，并不会存入数据库，用以代表当前是哪一步
   validates_presence_of :name, :email, :cellphone, :if => :should_validate_basic_data?
@@ -19,6 +20,12 @@ class Registration < ApplicationRecord
   end
 
   private
+  
+  def check_event_status
+    if self.event.status == "draft"
+      errors.add(:base, "活动尚未开放报名")
+    end
+  end
 
   def generate_uuid
     self.uuid = SecureRandom.uuid
@@ -31,5 +38,7 @@ class Registration < ApplicationRecord
   def should_validate_all_data?
     current_step == 3 || status == "confirmed"  # 做到第三步，或最后状态是 confirmed 时需要验证
   end
+
+
 
 end
